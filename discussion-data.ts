d036bdc4e2326fa99328b3675fe77abc1dfb2bb1
sazzad karim <97055612+mr-sazzad@users.noreply.github.com>
2024-03-22 21:05:10 +0600

    Update discussion-data.ts

--- discussion-data.ts
+++ discussion-data.ts
@@ -1,1 +1,67 @@
-
+export const discussionData = [
+  {
+    author: {
+      name: "John",
+      img: "https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg",
+      sector: 1,
+    },
+    post: {
+      data: "lorem ipsum dolor sit amet lore mauris et justo sit amet ipsum dolor sit amet loremauris et justo sit amet ipsum dolor sit amet justo sit amet ",
+      likes: "2k",
+      watch: "2k",
+      comments: "1k",
+    },
+  },
+  {
+    author: {
+      name: "John",
+      img: "https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg",
+      sector: 1,
+    },
+    post: {
+      data: "lorem ipsum dolor sit amet lore mauris et justo sit amet ipsum dolor sit amet loremauris et justo sit amet ipsum dolor sit amet justo sit amet ",
+      likes: "2k",
+      watch: "2k",
+      comments: "1k",
+    },
+  },
+  {
+    author: {
+      name: "John",
+      img: "https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg",
+      sector: 1,
+    },
+    post: {
+      data: "lorem ipsum dolor sit amet lore mauris et justo sit amet ipsum dolor sit amet loremauris et justo sit amet ipsum dolor sit amet justo sit amet ",
+      likes: "2k",
+      watch: "2k",
+      comments: "1k",
+    },
+  },
+  {
+    author: {
+      name: "John",
+      img: "https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg",
+      sector: 1,
+    },
+    post: {
+      data: "lorem ipsum dolor sit amet lore mauris et justo sit amet ipsum dolor sit amet loremauris et justo sit amet ipsum dolor sit amet justo sit amet ",
+      likes: "2k",
+      watch: "2k",
+      comments: "1k",
+    },
+  },
+  {
+    author: {
+      name: "John",
+      img: "https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg",
+      sector: 1,
+    },
+    post: {
+      data: "lorem ipsum dolor sit amet lore mauris et justo sit amet ipsum dolor sit amet loremauris et justo sit amet ipsum dolor sit amet justo sit amet ",
+      likes: "2k",
+      watch: "2k",
+      comments: "1k",
+    },
+  },
+];
